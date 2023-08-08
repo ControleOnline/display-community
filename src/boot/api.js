95b9248d88776b8b1cd32f1e774d89f9c7399833
Luiz Kim <luiz.kim@controleonline.com>
2023-08-08 13:22:42 -0300

    Update

--- src/boot/api.js
+++ src/boot/api.js
@@ -1,29 +1,46 @@
 import myFetch from "@controleonline/quasar-common-ui/src/utils/fetch";
+import axios from "axios";
 
 let myStore = null;
-
+const MIME_TYPE = "application/ld+json";
 export const api = {
-  fetch: function (id, options = {}) {
+  fetch: function (uri, options = {}) {
     if (typeof options.headers === "undefined")
       Object.assign(options, { headers: new Headers() });
-
-    if (
-      myStore.getters["auth/user"] !== null &&
-      myStore.getters["auth/user"].token
-    )
+    if (myStore.getters["auth/user"] && myStore.getters["auth/user"].token)
       options.headers.set("API-TOKEN", myStore.getters["auth/user"].token);
+    options.headers.set("Content-Type", MIME_TYPE);
+    options.headers.set("Accept", MIME_TYPE);
 
-    let myPromisse = myFetch(id, options);
+    if (options.body && typeof options.body != "string") {
+      options.body = JSON.stringify(options.body);
+    }
+    if (options.params) {
+      let params = [];
 
-    myPromisse.catch((e) => {
+      Object.keys(options.params).map((key) => {
+        if (Array.isArray(options.params[key])) {
+          params.push(
+            options.params[key].map((value) => `${key}[]=${value}`).join("&")
+          );
+        } else {
+          params.push(key + "=" + options.params[key]);
+        }
+      });
+      uri = `${uri}?${params.join("&")}`;
+    }
+
+    return myFetch(uri, options).catch((e) => {
       if (e.message == "Unauthorized" || e.message == "Invalid credentials.") {
         myStore.dispatch("auth/logOut");
-        localStorage.clear();
+        localStorage.set("session", null);
         location.reload();
       }
     });
+  },
 
-    return myPromisse;
+  execute: function (params) {
+    return axios(params);
   },
 };
 
