87789319db448378503ce7af2bb68f12d9f816f6
LEANDRO CUNHA <leandrocunha@MacBook-Air-de-LEANDRO.local>
2023-08-30 12:52:39 -0300

    Fixed bugs

--- quasar.conf.js
+++ quasar.conf.js
@@ -111,17 +111,17 @@ module.exports = function (ctx) {
       // Options below are automatically set depending on the env, set them if you want to override
       // preloadChunks: false,
       // extractCSS: false,
-      chainWebpack(chain) {
-        chain.module
-          .rule('css')
-          .oneOf('normal')
-          .use('css-loader')
-          .tap(options => {
-            // Ajuste as opções do css-loader aqui
-            options.url = false; // ou true, dependendo do que você precisa
-            return options;
-          });
-      },
+      // chainWebpack(chain) {
+      //   chain.module
+      //     .rule('css')
+      //     .oneOf('normal')
+      //     .use('css-loader')
+      //     .tap(options => {
+      //       // Ajuste as opções do css-loader aqui
+      //       options.url = false; // ou true, dependendo do que você precisa
+      //       return options;
+      //     });
+      // },
       // https://quasar.dev/quasar-cli/cli-documentation/handling-webpack
       extendWebpack(cfg) {},
     },
