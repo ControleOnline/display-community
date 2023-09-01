190797362c0b2dd45e13dde97f2bb5e818785789
Luiz Kim <luiz.kim@controleonline.com>
2023-08-31 22:14:10 -0300

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,11 @@
-import { LocalStorage } from "quasar";
-import { route } from "quasar/wrappers";
-import {
-  createRouter,
-  createMemoryHistory,
-  createWebHistory,
-  createWebHashHistory,
-} from "vue-router";
+import Vue from "vue";
+import VueRouter from "vue-router";
+
 import routes from "./routes";
+import { version } from "../../package.json";
+import { LocalStorage } from "quasar";
+
+Vue.use(VueRouter);
 
 /*
  * If not building with SSR mode, you can
@@ -17,23 +16,15 @@ import routes from "./routes";
  * with the Router instance.
  */
 
-export default route(function ( { store, ssrContext } ) {
-  const createHistory = process.env.SERVER
-    ? createMemoryHistory
-    : process.env.VUE_ROUTER_MODE === "history"
-    ? createWebHistory
-    : createWebHashHistory;
-
-  const Router = createRouter({
-    scrollBehavior: () => ({ left: 0, top: 0 }),
-    routes,
-
-    // Leave this as is and make changes in quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
-    history: createHistory(
-      process.env.MODE === "ssr" ? void 0 : process.env.VUE_ROUTER_BASE
-    ),
+export default function ({ store }) {
+  const Router = new VueRouter({
+    routes: routes,
+    mode: process.env.VUE_ROUTER_MODE,
+    base: process.env.VUE_ROUTER_BASE,
+    scrollBehavior: () => ({
+      x: 0,
+      y: 0,
+    }),
   });
 
   const autoLogin = () => {
@@ -44,6 +35,7 @@ export default route(function ( { store, ssrContext } ) {
     ) {
       return true;
     }
+
     // clean storage from not allowed keys
 
     let keys = LocalStorage.getAllKeys();
@@ -54,15 +46,13 @@ export default route(function ( { store, ssrContext } ) {
     if (LocalStorage.has("session")) {
       let session = LocalStorage.getItem("session");
 
-      // in case app version changes clear LocalStorage
-
       if (session.user != undefined) {
         store.dispatch("auth/logIn", {
           username: session.user,
           api_key: session.token,
           people: session.people,
           company: session.company,
-          
+          version: version,
           email: session.email,
           phone: session.phone,
           avatar: session.avatar,
@@ -73,47 +63,42 @@ export default route(function ( { store, ssrContext } ) {
 
         return true;
       }
-    } 
+    }
 
     return false;
   };
 
   Router.beforeEach((to, from, next) => {
-    const isLoginPage = to.path == "/login";
-    const isHomePage = to.path == "/";
-    const publicPages = ["/login", "/quote", "/shop"];
-    const isPrivatePage = !publicPages.includes(to.path);
+    const isLoginPage = to.name == "LoginIndex";
+
+    const publicPages = [
+      "LoginIndex",
+      "ChecklistDetails",
+      "ContractAccept",
+      "ProductsInCategory",
+      "CategoriesIndex",
+      "ProductDetails",
+      "QuoteIndex",
+      "ShopIndex",
+      "ForgotPassword",
+    ];
+    const isPrivatePage = !publicPages.includes(to.name);
     const isLogged = autoLogin();
 
-    // ------ /task/checklist/id/{integer}
-    if (to.name === "ChecklistDetails" || to.name === "ContractAccept") {
-      // Para não redirecionar para página de login ao abrir vistoria sem estar logado
-      return next();
-    }
-
-    //sub router for /shop
-    if (
-      to.name === "ProductsInCategory" ||
-      to.name === "CategoriesIndex" ||
-      to.name === "ProductDetails"
-    ) {
-      return next();
-    }
-
-    if (to.path.match(/^\/forgot-password\/[\w\W]+\/[\w\W]+$/g)) {
-      return next();
-    }
-
-    if ((isLoginPage || isHomePage) && isLogged) {
+    if ((isLoginPage && isLogged) || to.name == undefined) {
       return next({ name: "HomeIndex" });
     }
 
-    if (isPrivatePage === true && isLogged === false) {
-      return next("/login");
+    if (
+      isPrivatePage === true &&
+      isLogged === false &&
+      to.name != "LoginIndex"
+    ) {
+      return next({ name: "LoginIndex" });
     }
 
-    next();
+    return next();
   });
 
   return Router;
-});
+}
